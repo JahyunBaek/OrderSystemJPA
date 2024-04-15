2fb7a9f4fed749c329c6cd422c3830684caa51b0
BJH <baekjahyun@nongshim.co.kr>
2024-04-15 16:52:23 +0900

    [INIT] Entity 생성

---
+++ src/main/java/com/example/jpa/demo/domain/DeliveryStatus.java
@@ -0,0 +1,5 @@
+package com.example.jpa.demo.domain;
+
+public enum DeliveryStatus {
+    READY, COMP
+}
---
+++ src/main/java/com/example/jpa/demo/domain/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.example.jpa.demo.domain;
+
+public enum OrderStatus {
+    ORDER, CANCEL
+}
