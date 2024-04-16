e1d896dd2704fa32bb18664f6014cabb553af756
BJH <baekjahyun@nongshim.co.kr>
2024-04-16 13:48:15 +0900

    [ADD] 컬렉션 조회 최적화

--- src/main/java/com/example/jpa/demo/DemoApplication.java
+++ src/main/java/com/example/jpa/demo/DemoApplication.java
@@ -2,6 +2,10 @@ package com.example.jpa.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
+
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,5 +13,16 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	@Bean
+    Hibernate5JakartaModule hibernate5Module() {
+        return new Hibernate5JakartaModule();
+    }
 
+    // 강제 지연 로딩 설정을 하기 위해서는 이와 같이 추가해주면 된다.
+    @Bean
+    Hibernate5JakartaModule hibernate5JakartaModule() {
+        Hibernate5JakartaModule hibernate5JakartaModule = new Hibernate5JakartaModule();
+        hibernate5JakartaModule.configure(Hibernate5JakartaModule.Feature.FORCE_LAZY_LOADING, true);
+        return hibernate5JakartaModule;
+    }
 }
---
+++ src/main/java/com/example/jpa/demo/repository/OrderSearch.java
@@ -0,0 +1,13 @@
+package com.example.jpa.demo.repository;
+
+import com.example.jpa.demo.domain.OrderStatus;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class OrderSearch {
+
+    private String memberName; //회원 이름
+    private OrderStatus orderStatus; //주문 상태[ORDER, CANCEL]
+}
